6b78197a0c0f3e1be53d7b8b40c00f430a35fe14
pikaqiuzhisha <55611651+pikaqiuzhisha@users.noreply.github.com>
2020-07-16 10:28:10 +0800

    Delete cleanLastUpdated.bat

--- mavn/cleanLastUpdated.bat
+++
@@ -1,7 +0,0 @@
-set REPOSITORY_PATH=G:\maven\apache-maven-3.5.2\repository
-rem ÕýÔÚËÑË÷...
-for /f "delims=" %%i in ('dir /b /s "%REPOSITORY_PATH%\*lastUpdated*"') do (
-    del /s /q %%i
-)
-rem ËÑË÷Íê±Ï
-pause
